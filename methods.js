1ebe1d669368917beb09f97040b7c060ed603379
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2023-10-11 18:01:36 +0530

    update account addresses

--- methods.js
+++ methods.js
@@ -1,14 +1,27 @@
 import {exec} from 'child_process';
 
 //constants
-const tokenAddress = 'AUNnuUwLwgYZ3JPUYc23awDqcuHhDootHVkY1T3UoKQy'
+// const tokenAddress = 'AUNnuUwLwgYZ3JPUYc23awDqcuHhDootHVkY1T3UoKQy'
+// const tokenDecimals = 1
+// const tokenAccount = 'GcZ4WrjePK4giQEV8H3jTxd71uvsqyhwvN11rTNyhAnz'
+
+const tokenAddress = 'ERW1cH6jbFd6P3sdZoH9QdgqjCvVf5RTMrzEWSWXaEto'
 const tokenDecimals = 1
-const tokenAccount = 'GcZ4WrjePK4giQEV8H3jTxd71uvsqyhwvN11rTNyhAnz'
+const tokenAccount = '2BDDpmbZeVx9oTkAfm31diBb1e37jF3rEoRrFFR5zQ6e'
 
 //address: AUNnuUwLwgYZ3JPUYc23awDqcuHhDootHVkY1T3UoKQy
 // signature: 3tEHUdmb1ZnkQLL5HeN7JHR86LrTi3f8UqAwQAotDWaknRMmic5C4Y5GQFAKX1ux1ugDT9buxJv4NhfinwxKowyD
 // mint account: GcZ4WrjePK4giQEV8H3jTxd71uvsqyhwvN11rTNyhAnz
 // mint account signature: 43dWQ9azqT1oi7C7t6FWPzW6vJqQQvyrMrkzWpSvHBinyzWHwYsNvUj6sZE3sT1XEbyBV9hNb
+
+
+
+//new token
+// metaboss create fungible -d 1 -m metadata.json --initial-supply 100
+// Signature: 2mUbCoXj2pnWQvQxbchbRRdi2PCLQx73N4ozTftvd97yQMurX4aJLiV4FZZJjygfA5s6pzc1BMthYjos11DXqU2W
+// Mint: ERW1cH6jbFd6P3sdZoH9QdgqjCvVf5RTMrzEWSWXaEto
+// Metadata: BNASfbjwSmHM6daFC7yzb3LTmSGAruLrQWdEWsPcGQPU
+// token account for the mint: 2BDDpmbZeVx9oTkAfm31diBb1e37jF3rEoRrFFR5zQ6e
 
 export async function mint(walletAddress, amount) {
     //use executeCommand instead of runCliCommand
